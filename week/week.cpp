e81aa0171581bda8cf1be772cb12d7ff2025f1a4
Sanjeet Kumar <sanjeetkum960@gmail.com>
2024-11-02 01:31:48 +0530

    day of week

---
+++ week/week.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int num;
+    cin>>num;
+    if(num==1)
+    cout<<"monday";
+    else if(num==2)
+    cout<<"tuesday";
+    else if(num==3)
+    cout<<"wednesday";
+    else if(num==4)
+    cout<<"thrusday";
+    else if(num==5)
+    cout<<"friday";
+    else if(num==6)
+    cout<<"saturday";
+    else if(num==7)
+    cout<<"sunday";
+    else 
+    cout<<"valid day";
+}
+
+
+
+
+
+
